3f42229495ce5411bd57ec45bf434c9c300d96a4
Vadym Zhufyak <zhufyakvv@gmail.com>
2017-12-10 14:28:33 +0200

    Initial. 5 labs.

---
+++ Lab1/Lab1/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Person
+    {
+        private String first_name;
+        private String last_name;
+        private DateTime birthday;
+
+        public Person(String f_n, String l_n, DateTime bd) {
+            this.first_name = f_n;
+            this.last_name = l_n;
+            this.birthday = bd;
+        }
+
+        public Person() {
+            this.first_name = "Dima";
+            this.last_name = "Fagurel";
+            this.birthday = new DateTime(1997, 3, 11);
+        }
+
+        public string getFirstName() {return this.first_name;}
+        public void setFirstName(String value) {this.first_name = value;}
+
+        public DateTime getBirthday() { return this.birthday; }
+        public void setBirthday(DateTime value) { this.birthday = value; }
+
+        public string getLastName() { return this.last_name; }
+        public void setLastName(String value) { this.last_name = value; }
+
+        public int Age {
+            get { return birthday.Year; }
+            set { birthday = new DateTime(value, birthday.Month, birthday.Day); }
+        }
+
+        public override String ToString() {
+            return String.Format("Person: {0}  {1},\n\tBirthDay => {2}" ,first_name , last_name, birthday.ToString("yyyy/MM/dd"));
+        }
+
+        public virtual String ToShortString() {
+            return this.first_name + " " + this.last_name;
+        }
+    }
+
+}
---
+++ Lab2/Lab2/Magazine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    class Magazine : Edition {
+        private Frequency frequency;
+        private List<Article> articles = new List<Article>();
+        private List<Person> editors = new List<Person>();
+
+        public Magazine(String n, Frequency f, DateTime d, int c): base(n, d, c) {
+            this.frequency = f;
+        }
+
+        public Magazine():this("Time", Frequency.Montly, DateTime.Today, 1) {}
+
+        public Frequency getFrequency() {
+            return this.frequency;
+        }
+        public void setFrequency(Frequency f) {
+            this.frequency = f;
+        }
+
+        public double AvgRating {
+            get {
+                Double total = 0;
+                int length = this.articles.Count;
+                if (length == 0) return 0;
+                foreach(Article a in this.articles) {
+                    total += a.rating;
+                }
+                return total / length;
+            }
+        }
+
+        public List<Article> ArticlesList {
+            get { return this.articles; }
+        }
+
+        public List<Person> EditorsList {
+            get { return this.editors; }
+        }
+
+        public void addArticles(List<Article> new_articles) {
+            foreach (Article a in new_articles)
+                this.articles.Add(a);
+        }
+
+        public void addEditors(List<Person> new_editors) {
+            foreach (Person p in new_editors)
+                this.editors.Add(p);
+        }
+
+        public override String ToString() {
+            String res = base.ToString();
+            res += $"Frequency={frequency}\nArticles=>\n";
+            foreach (Article a in articles)
+                res += $"\t{a.ToString()}\n";
+            res += "Editors=>\n";
+            foreach (Person e in editors)
+                res += $"\t{e.ToString()}\n";
+            return res;
+    }
+
+        public virtual String ToShortString() {
+            String res = base.ToString();
+            res += $"Frequency={frequency}\nAverage Rating={AvgRating}\n";
+            return res;
+        }
+
+        static void Main(string[] args) {
+            Magazine m = new Magazine();
+            List<Article> articles = new List<Article> { new Article() };
+            List<Person> editors = new List<Person> { new Person() };
+            m.addArticles(articles);
+            m.addEditors(editors);
+            Console.WriteLine(m.ToShortString());
+            Console.WriteLine(m.ToString()); 
+            Console.ReadLine();
+
+        }
+
+    }
+}
